83d25bc1e6fce57f4d78ec8917f381b6f41abd8a
somesi <146617020+somesi@users.noreply.github.com>
2024-04-05 23:28:41 +0500

    no sudo

--- firewalld.sh
+++ firewalld.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if systemctl is-active --quiet firewalld.service; then
-    sudo firewall-cmd --zone=FedoraWorkstation --add-masquerade --permanent
-    sudo firewall-cmd --zone=trusted --add-masquerade --permanent
-    sudo firewall-cmd --zone=trusted --add-interface=ap0 --permanent
+    firewall-cmd --zone=FedoraWorkstation --add-masquerade --permanent
+    firewall-cmd --zone=trusted --add-masquerade --permanent
+    firewall-cmd --zone=trusted --add-interface=ap0 --permanent
 else
     true
 fi
