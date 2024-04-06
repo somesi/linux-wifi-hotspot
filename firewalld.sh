0d35abf480cecbdb65d606cead8ba5da90a1fcd2
somesi <sidneypro@proton.me>
2024-04-06 05:50:25 +0500

    Revert "test"
    This reverts commit b4500b64c1148c4413f1be38213d4b5e5011b890.

--- firewalld.sh
+++ firewalld.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if true; then
+if systemctl is-active --quiet firewalld.service; then
     firewall-cmd --zone=FedoraWorkstation --add-masquerade --permanent
     firewall-cmd --zone=trusted --add-masquerade --permanent
     firewall-cmd --zone=trusted --add-interface=ap0 --permanent
