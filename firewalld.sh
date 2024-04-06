9eac56949c2e7fbaa15b03023fbcb41072132636
somesi <sidneypro@proton.me>
2024-04-07 04:50:10 +0500

    no longer needed

--- firewalld.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if systemctl is-active --quiet firewalld.service; then
-    firewall-cmd --zone=FedoraWorkstation --add-masquerade --permanent
-    firewall-cmd --zone=trusted --add-masquerade --permanent
-    firewall-cmd --zone=trusted --add-interface=ap0 --permanent
-else
-    true
-fi
